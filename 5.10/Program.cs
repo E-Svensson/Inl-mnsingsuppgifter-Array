edc249ff2812e40f3dbb5c8861d1ae97b927d9a2
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-27 11:11:54 +0100

    gsrgssgsg

--- 5.10/Program.cs
+++ 5.10/Program.cs
@@ -7,32 +7,22 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Skriv in flera heltal men mellanslag som mellanrum: "); string input = Console.ReadLine();
+            Console.Write("Skriv in flera heltal med mellanslag som mellanrum: "); string input = Console.ReadLine();
 
-            string[] inputArray = input.Split(" ");
-            int[] ArrayInteger = new int[inputArray.Length];
-
-            Array.Sort(inputArray);
+            string[] inputArray = input.Trim().Split();
+            int[] ArrayAntal = new int[inputArray.Length];
+            int flestAntal = 1;
 
             for (int i = 0; i < inputArray.Length; i++)
             {
                 for (int j = 0; j < inputArray.Length; j++)
                 {
-                    if (inputArray[i].Contains(inputArray[j]))
+                    if (inputArray[i] == inputArray[j])
                     {
-                        ArrayInteger[i]++;
+                        ArrayAntal[i]++;
                     }
                 }
-            }
-
-            Console.Write("\nFöljande tal är vanligast: ");
-
-            for (int i = 0; i < ArrayInteger.Length; i++)
-            {
-                if (ArrayInteger[i] != 1)
-                {
-                    Console.Write(inputArray[i]);
-                }
+                Console.WriteLine(ArrayAntal[i]);
             }
         }
     }
